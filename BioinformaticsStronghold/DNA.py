9b0337897da9ffc951bc7a4f092f991358b8bc18
Kohl Kinning <kohl@Kohls-MBP.domain>
2017-05-12 15:04:15 -0700

    Counting DNA Nucleotides

---
+++ BioinformaticsStronghold/DNA.py
@@ -0,0 +1,16 @@
+print "Given: A DNA string s of length at most 1000 nt. Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.\n"
+
+import collections
+
+print "String: "
+seq = str(raw_input())
+
+d = collections.defaultdict(int)
+for c in seq:
+    d[c] += 1
+
+
+for key, value in d.items():
+    print value,        #, importantly replaces \n with space
+
+
